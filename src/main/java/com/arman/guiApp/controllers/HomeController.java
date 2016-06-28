7aadfbf176e6387369ac95c45963d1261a54fc9e
Arman Kostandyan <armankostandyan@gmail.com>
2016-06-29 00:26:37 +0400

    Added Charts

--- src/main/java/com/arman/guiApp/controllers/HomeController.java
+++ src/main/java/com/arman/guiApp/controllers/HomeController.java
@@ -6,6 +6,9 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.PieChart;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
@@ -31,9 +34,18 @@ public class HomeController implements Initializable {
     private TableColumn<User, Integer> ageCol;
     @FXML
     private TableColumn<User, String> emailCol;
+    @FXML
+    private PieChart pieChart;
+    @FXML
+    private LineChart<String,Number> lineChart;
 
-    private final ObservableList<String> items = FXCollections.observableArrayList(
+    private final static ObservableList<String> ITEMS = FXCollections.observableArrayList(
             "Item 1", "Item 2", "Item 3", "Item 4");
+    private final static ObservableList<PieChart.Data> PIE_CHART_DATA
+            = FXCollections.observableArrayList(
+                    new PieChart.Data("10 - 19", 33),
+                    new PieChart.Data("20 - 29", 50),
+                    new PieChart.Data("30 - 39", 17));
 
     /**
      * Initializes the controller class.
@@ -43,14 +55,45 @@ public class HomeController implements Initializable {
      */
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        itemsList.setItems(items);
+        initTable();
+        initPieChart();
+        initLineChart();
+    }
+
+    private void initTable() {
+        itemsList.setItems(ITEMS);
         idCol.setCellValueFactory(cellData -> cellData.getValue().idProperty());
         fullNameCol.setCellValueFactory(cellData -> cellData.getValue().fullNameProperty());
         ageCol.setCellValueFactory(new PropertyValueFactory("age"));
         emailCol.setCellValueFactory(cellData -> cellData.getValue().emailProperty());
-        
+
         ObservableList<User> users = User.dummyUser();
         table.setItems(users);
+    }
+
+    private void initPieChart() {
+        pieChart.setData(PIE_CHART_DATA);
+        pieChart.setLabelsVisible(true);
+    }
+
+    private void initLineChart() {
+        XYChart.Series series = new XYChart.Series();
+        series.setName("Monthly close");
+        
+        series.getData().add(new XYChart.Data("Jan", 23));
+        series.getData().add(new XYChart.Data("Feb", 14));
+        series.getData().add(new XYChart.Data("Mar", 15));
+        series.getData().add(new XYChart.Data("Apr", 24));
+        series.getData().add(new XYChart.Data("May", 34));
+        series.getData().add(new XYChart.Data("Jun", 36));
+        series.getData().add(new XYChart.Data("Jul", 22));
+        series.getData().add(new XYChart.Data("Aug", 45));
+        series.getData().add(new XYChart.Data("Sep", 43));
+        series.getData().add(new XYChart.Data("Oct", 17));
+        series.getData().add(new XYChart.Data("Nov", 29));
+        series.getData().add(new XYChart.Data("Dec", 25));
+        
+        lineChart.getData().add(series);
     }
 
 }
